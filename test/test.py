9c472df3af7ed7dba3a54444252f767b2a19ce9d
Mat <matstaryt@gmail.com>
2024-11-19 21:32:38 +0100

    Added Unit Testing

--- test/test.py
+++ test/test.py
@@ -1,26 +1,29 @@
+import unittest
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from compare import *
 from timecompare import *
 
-def test_compare():
-    write('rat', '2021-08-01', 'rat_followers.txt', ['a', 'b', 'c', 'd', 'e'])
-    print(load('rat', '2021-08-01', 'rat_followers.txt'))
 
-def test_time_compare():
-    write('rat', '2021-08-01', 'rat_followers.txt', ['a', 'b', 'c', 'd', 'lost'])
-    write('rat', '2021-08-01', 'rat_following.txt', ['a', 'b', 'c', 'd', 'stopped'])
-    write('rat', '2021-08-02', 'rat_followers.txt', ['a', 'b','gained' ,'c', 'd'])
-    write('rat', '2021-08-02', 'rat_following.txt', ['a', 'b', 'started', 'c', 'd'])
+class TestWriteLoad(unittest.TestCase):
+    def test_write_load(self):
+        test_users = {'rat', 'cat', 'bat', 'mat'}
+        write('rat', '2021-08-01', 'rat_followers.txt', test_users)
+        self.assertEqual(load('rat', '2021-08-01', 'rat_followers.txt'), test_users)
 
-    timeCompare('rat', '2021-08-01', '2021-08-02')
+class TestTimeCompare(unittest.TestCase):
+    def test_time_compare(self):
+        write('rat', '2021-08-01', 'rat_followers.txt', {'a', 'b', 'c', 'd', 'lost'})
+        write('rat', '2021-08-01', 'rat_following.txt', {'a', 'b', 'c', 'd', 'stopped'})
+        write('rat', '2021-08-02', 'rat_followers.txt', {'a', 'b', 'gained', 'c', 'd'})
+        write('rat', '2021-08-02', 'rat_following.txt', {'a', 'b', 'started', 'c', 'd'})
 
-    print("[INFO] - Gained Followers: ", load('rat', '2021-08-02', '2021-08-01_gained_followers.txt'))
-    print("[INFO] - Lost Followers: ",load('rat', '2021-08-02', '2021-08-01_lost_followers.txt'))
-    print("[INFO] - Started Following: ",load('rat', '2021-08-02', '2021-08-01_started_following.txt'))
-    print("[INFO] -Stopped Following: ",load('rat', '2021-08-02', '2021-08-01_stopped_following.txt'))
-    pass
+        timeCompare('rat', '2021-08-01', '2021-08-02')
+
+        self.assertEqual(load('rat', '2021-08-02', '2021-08-01_lost_followers.txt'), {'lost'})
+        self.assertEqual(load('rat', '2021-08-02', '2021-08-01_gained_followers.txt'), {'gained'})
+        self.assertEqual(load('rat', '2021-08-02', '2021-08-01_stopped_following.txt'), {'stopped'})
+        self.assertEqual(load('rat', '2021-08-02', '2021-08-01_started_following.txt'), {'started'})
 
 if __name__ == '__main__':
-    test_compare()
-    test_time_compare()
+    unittest.main()
